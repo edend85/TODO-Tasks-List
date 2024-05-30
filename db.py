cdf3a7e4c4118ab96829b63d8a694e6d0de7a795
edend85 <117260021+edend85@users.noreply.github.com>
2024-05-30 16:41:16 +0300

    Update db.py

--- db.py
+++ db.py
@@ -11,6 +11,7 @@ class DataBase:
             f"DATABASE={os.getenv('DB_NAME')};"
             f"UID={os.getenv('DB_UID')};"
             f"PWD={os.getenv('DB_PWD')};"
+            f"TrustServerCertificate=yes;"
         )
     def _connect(self):
         return pyodbc.connect(self.connection_string)
