f859769556058397fbb86e748c9cc1d424924f9e
edend85 <117260021+edend85@users.noreply.github.com>
2024-06-04 16:51:15 +0300

    Create wsgi.py

---
+++ wsgi.py
@@ -0,0 +1,8 @@
+from app import app
+
+if __name__ == "__main__":
+    from waitress import serve
+    import os
+
+    port = int(os.environ.get("PORT", 3000))
+    serve(app, host='0.0.0.0', port=port)
